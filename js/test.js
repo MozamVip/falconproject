75e5c4f6a4bdb7b619ecdaffb0e6f63dc6fe9d4f
MozamVip <84370388+MozamVip@users.noreply.github.com>
2024-09-19 08:50:59 -0700

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,41 @@
+"biochemistry": {
+        "easy": [
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            
+        ],
+        "medium": [
+            { title: "  ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            
+        ],
+        "difficult": [
+            { title: "  ", description: "  .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            { title: " ", description: " .", link: "https://wa.me/2348071272293" },
+            
+        ]
+    },
